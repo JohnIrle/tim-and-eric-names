a74410e7efc01e980aa7f55a56be14830fffc362
John Irle <irlejohn@gmail.com>
2022-06-17 23:01:58 -0500

    refactor: update code and packages. (#25)
    * chore: run audit fix.
    
    * chore: update mocha and chai.
    
    * chore: remove unused badges
    
    * chore: update webpack and webpack-cli.
    
    * chore: update commitizen and cz-conventional-changelog
    
    * chore: remove duplicate config
    
    * chore: update babel, semantic release. Remove unused config.
    
    * chore: update babel-loader.
    
    * ci: remove coverage and add build.
    
    * chore: update babel packages.
    
    * chore: remove unused scripts.
    
    * chore: uninstall nyc, istanbul.
    
    * chore: remove mocha and use jest.
    
    * test: update test to support jest matchers.
    
    * refactor: use const, let, remove unnecessary else.
    
    * refactor: json-loaded no long required.
    
    * ci: add publish workflow.
    
    * chore: bump version.

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,21 +1,16 @@
-module: {
-  rules: [
-    {
-      test: /\.js$/,
-      exclude: /(node_modules)/,
-      use: {
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-env"]
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
         }
       }
-    },
-    {
-      test: /\.json$/,
-      exclude: /(node_modules)/,
-      use: {
-        loader: "json-loader"
-      }
-    }
-  ];
+    ]
+  }
 }
